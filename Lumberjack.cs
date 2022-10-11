a390004cf2e98e4ea25e30f3564a9ed21856c379
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-10-10 21:32:34 -0500

    Add project files.

---
+++ Lumberjack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lumberjacks
+{
+    internal class Lumberjack
+    {
+        public string Name { get; private set; }
+        private Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+        public Lumberjack(string name)
+        {
+            Name = name;
+        }
+
+        public void TakeFlapJack(Flapjack flapjack)
+        {
+            flapjackStack.Push(flapjack);
+        }
+
+        public void EatFlapjacks()
+        {
+            Console.WriteLine($"{Name} is eating flapjacks");
+            while (flapjackStack.Count != 0)
+            {
+            Flapjack lastFlapJack = flapjackStack.Pop();
+            Console.WriteLine($"{Name} ate a {lastFlapJack} flapjack");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Lumberjacks
+{
+    internal class Program
+    {
+        static Random random = new Random();
+        static Queue<Lumberjack> lumberjackQueue = new Queue<Lumberjack>();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                string consoleWrite = lumberjackQueue.Count == 0 ? "First lumberjack's name: " : "Next lumberjacks's name (blank to end): ";
+                Console.Write(consoleWrite);
+                string lumberjackName = Console.ReadLine();
+                if (lumberjackName != "")
+                {
+                    Lumberjack lumberjack = new Lumberjack(lumberjackName);
+                    Console.Write("Number of flapjacks: ");
+                    if (int.TryParse(Console.ReadLine(), out int numberOfFlapjacks))
+                    {
+                        for (int i = 0; i < numberOfFlapjacks; i++)
+                        {
+                            lumberjack.TakeFlapJack((Flapjack)random.Next(0, 4));
+                        }
+
+                    }
+                    lumberjackQueue.Enqueue(lumberjack);
+
+                }
+                else
+                {
+                    while (lumberjackQueue.Count != 0)
+                    {
+
+                        lumberjackQueue.Dequeue().EatFlapjacks();
+                    }
+                    return;
+                }
+
+            }
+
+
+        }
+    }
+}
